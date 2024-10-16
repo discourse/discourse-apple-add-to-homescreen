65688bb4f01c85add5a849e6c80776d8388ecf15
Kelvin Tan <kelv@discourse.org>
2024-10-16 18:45:18 +0800

    DEV: Update linting setup

--- javascripts/discourse/components/add-to-homescreen.js
+++ javascripts/discourse/components/add-to-homescreen.js
@@ -1,7 +1,7 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import discourseLater from "discourse-common/lib/later";
 import { bind } from "discourse-common/utils/decorators";
 import I18n from "I18n";
