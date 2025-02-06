8437032ba8d0870ed45c7ff5797427d4895cc54a
David Taylor <david@taylorhq.com>
2025-02-06 13:49:38 +0000

    DEV: Bump dependencies and fix linting

--- javascripts/discourse/components/add-to-homescreen.js
+++ javascripts/discourse/components/add-to-homescreen.js
@@ -2,9 +2,9 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { service } from "@ember/service";
-import discourseLater from "discourse-common/lib/later";
-import { bind } from "discourse-common/utils/decorators";
-import I18n from "I18n";
+import { bind } from "discourse/lib/decorators";
+import discourseLater from "discourse/lib/later";
+import { i18n } from "discourse-i18n";
 
 export default class AddToHomescreen extends Component {
   @service capabilities;
@@ -50,7 +50,7 @@ export default class AddToHomescreen extends Component {
   }
 
   get PWALabel() {
-    return I18n.t(themePrefix("pwa_text"), {
+    return i18n(themePrefix("pwa_text"), {
       siteTitle: this.site.siteSettings.title,
     });
   }
